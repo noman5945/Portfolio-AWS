f6c33a0e3355e1475df369c7700d6be6801451e7
noman5945 <abdullahnoman.5945@gmail.com>
2026-09-13 18:42:14 +0200

    [UPDATE] Updated LinkedIn link and CV

--- app/pages/contact/page.tsx
+++ app/pages/contact/page.tsx
@@ -33,7 +33,7 @@ export default function ContactPage() {
             Email Me
           </CustomButton>
 
-          <CustomButton href="https://www.linkedin.com/in/noman5945/?isSelfProfile=false">
+          <CustomButton href="https://www.linkedin.com/in/noman594527/">
             LinkedIn
           </CustomButton>
           <CustomButton href="https://github.com/noman5945" variant="outline">
